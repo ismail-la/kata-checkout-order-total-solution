98aff0e0215b50e3060db9076f332f67efd54270
ismail-la <lahbariismail@gmail.com>
2025-07-06 14:06:52 +0100

    RED: Add failing tests for per-unit item scanning

--- tests/checkout.test.ts
+++ tests/checkout.test.ts
@@ -1,5 +1,6 @@
 import { Checkout } from "../src/checkout";
 
+// Checkout system Custom Test Suite:
 describe("Checkout Order Total Kata", () => {
   describe("Basic functionality", () => {
     it("should initialize with zero total", () => {
@@ -7,4 +8,41 @@ describe("Checkout Order Total Kata", () => {
       expect(checkout.getTotal()).toBe(0);
     });
   });
+
+describe("Per-unit items", () => {
+  it("should add per-unit (single scanned item) item to total", () => {
+    // Arrange:
+    const checkout = new Checkout();
+    checkout.setPricePerUnit("soup", 1.89);
+
+    // Act:
+    checkout.scan("soup");
+
+    // Assert:
+    expect(checkout.getTotal()).toBe(1.89);
+  });
+
+  it("sums Total for multiple per-unit items ", () => {
+    // Arrange:
+    const checkout = new Checkout();
+    checkout.setPricePerUnit("soup", 1.89);
+    checkout.setPricePerUnit("bread", 2.5);
+
+    // Act:
+    checkout.scan("soup");
+    checkout.scan("bread");
+
+    // Assert:
+    expect(checkout.getTotal()).toBe(4.39);
+  });
+});
+
+
+
+
+
+
+
+
+
 });
